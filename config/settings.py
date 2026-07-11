6469d1dbb3fe9cd36a423729491b274d7d071010
Ckelyson <contatockelyson@gmail.com>
2026-07-11 14:46:03 -0300

    Configura banco PostgreSQL para deploy no Render

--- config/settings.py
+++ config/settings.py
@@ -129,24 +129,16 @@ TEMPLATES = [
 # No Render, será utilizada a variável DATABASE_URL.
 # Localmente, caso DATABASE_URL não exista, será usado SQLite.
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cim',
+        'USER': 'admin',
+        'PASSWORD': 'SUA_SENHA',
+        'HOST': 'dpg-d8rb58j7uimc73evgji0-a.ohio-postgres.render.com',
+        'PORT': '5432',
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 
 # =========================================================
